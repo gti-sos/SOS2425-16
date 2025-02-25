a866c7fb44e88b0948e6cdfa369bf7991bed0ea7
TheVaultBoy16 <gonaguiman@gmail.com>
2025-02-25 13:21:51 +0100

    si

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 16078;
 
 app.use("/",express.static("./public"));
+app.use("/about",express.static("./public/about.html"));
 
 app.get("/cool",(request,response)=>{
     response.send(cool());
 });
 
-app.get("/about",(request,response)=>{
-    app.use("/",express.static("./public/about.html"));
-});
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}...`);
