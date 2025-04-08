95a7be72981bcb884b77b4dedc435e79a459e527
TheVaultBoy16 <gonaguiman@gmail.com>
2025-04-08 21:26:05 +0200

    SvelteKit integrado

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import express from "express";
-import {loadBackendGAM} from "./src/back/index-GAM.js";
+import { loadBackendGAM } from "./src/back/index-GAM.js";
 import { loadBackendIBL } from "./src/back/index-IBL.js";
 import { loadBackendPVS } from "./src/back/index-PVS.js";
+import { handler } from "./src/front/build/handler.js";
 
 
 const app = express();
 const PORT = process.env.PORT || 16078;
 const BASE_API= "/api/v1";
 
-app.use("/",express.static("./public"));
-app.use("/about",express.static("./public/about.html"));
+//app.use("/",express.static("./public"));
+//app.use("/about",express.static("./public/about.html"));
 app.use(express.json());
-
-
-
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}...`);
@@ -30,3 +28,7 @@ loadBackendPVS(app);
 // index-IBL.js
 
 loadBackendIBL(app);
+
+// Loads the front-end after the back-end.
+
+app.use(handler);
