2c848086169849fa3d83e09df020b53f91f8fd4d
TheVaultBoy16 <gonaguiman@gmail.com>
2025-04-20 20:40:35 +0200

    Pagina principal actualizada

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -2,3 +2,21 @@
     <title>API_GRUPO_16</title>
 </svelte:head>
 <h1>Bienvenido a la API del grupo 16 de SOS</h1>
+
+<a href="https://sos2425-16.onrender.com/api/v1/emigration-stats"> API Emigración </a>
+<a href="https://sos2425-16.onrender.com/api/v1/taxes-stats"> API Impuestos</a>
+<a href="https://sos2425-16.onrender.com/api/v1/unemployment-stats"> API Desempleo</a>
+
+
+<a href="https://sos2425-16.onrender.com/api/v1/emigration-stats/docs"> Documentación API Emigración</a>
+<a href="https://sos2425-16.onrender.com/api/v1/taxes-stats/docs"> Documentación API Impuestos</a>
+<a href="https://sos2425-16.onrender.com/api/v1/unemployment-stats/docs"> Documentación API Desempleo</a>
+
+<a href="https://github.com/gti-sos/SOS2425-16"> GitHub del equipo</a>
+
+<h3>Nombre de los componentes del equipo y el nombre de su fuente de datos</h3>
+<p>Gonzalo Aguilera Mancheño: emigration-stats</p>
+<p>Iván Barbero Lay: taxes-stats</p>
+<p>Pablo Viña Santacruz: unemployment-stats</p>
+
+
