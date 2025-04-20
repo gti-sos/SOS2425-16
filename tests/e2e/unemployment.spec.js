2ffdb2f892eed951088fefdebac0d3397f533bf6
NFN8347 <pablovsc2004@gmail.com>
2025-04-21 00:45:53 +0200

    mañana sigo

--- tests/e2e/unemployment.spec.js
+++ tests/e2e/unemployment.spec.js
@@ -12,9 +12,31 @@ test('get emigration link', async ({ page }) => {
   await page.goto('localhost:16078');
 
   // Click the emigration link.
-  await page.getByRole('link', { name: 'Estadísticas sobre la emigración en España' }).click();
+  await page.getByRole('link', { name: 'Estadísticas sobre el desempleo en España' }).click();
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Desempleo en España/);
 });
 
+test('create and delete data', async ({ page }) => {
+    const testName = "__TEST_NAME__";
+    const testYear = "2020";
+    const testQuarter = "q1";
+    const testUnemployment_rate = "1234";
+    const testPrevious_quarter_var= "1212";
+    const testPrevious_year_quarter_var= "1111";
+
+    await page.goto('localhost:16078');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Estadísticas sobre el desempleo en España' }).click();
+
+    await page.getByPlaceholder('Comunidad Autónoma').fill(testName);
+    await page.getByPlaceholder('Inserte año').fill(testYear); // cambiar porque no es textbox y no se como localizarla
+    await page.getByPlaceholder('Trimestre').fill(testQuarter);
+    await page.getByPlaceholder('Tasa de desempleo').fill(testUnemployment_rate);
+    await page.getByPlaceholder('Var. trimestre anterior').fill(testPrevious_quarter_var);
+    await page.getByPlaceholder('Var. mismo trimestre año anterior').fill(testPrevious_year_quarter_var);
+
+    await page.getByRole('button', {name: "Añadir"}).click();
+});
