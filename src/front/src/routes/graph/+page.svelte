04b505acb4a9a54866f140cc29203dbff4b34d42
TheVaultBoy16 <gonaguiman@gmail.com>
2025-04-30 09:45:40 +0200

    Grafo con amcharts de emigracion

--- src/front/src/routes/graph/+page.svelte
+++ src/front/src/routes/graph/+page.svelte
@@ -1,1 +1,2 @@
-<a href ="/graph/emigration_graph">Grafo de emigración con highcharts</a>
+<a href ="/emigration_graph_highchart">Grafo de emigración con highcharts</a>
+<a href ="/emigration_graph_zingchart">Grafo de emigración con zingchart</a>
