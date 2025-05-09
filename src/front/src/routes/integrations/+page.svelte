4e31cfad4debcf91496285721820cceeb3b0e78c
TheVaultBoy16 <gonaguiman@gmail.com>
2025-05-09 09:46:11 +0200

    Arreglao pablo

--- src/front/src/routes/integrations/+page.svelte
+++ src/front/src/routes/integrations/+page.svelte
@@ -8,6 +8,3 @@
 <a href ="/integracion_OpenWeatherMap">Integración con la API de OpenWeatherMap</a>
 <a href ="/integracion_ExchangeRate">Integración con la API de ExchangeRate</a>
 <a href ="/integracion_covid-api">Integración con la API de datos covid-api</a>
-<a href ="/integracion_OpenWeatherMap">Integración con la API de OpenWeatherMap</a>
-<a href ="/integracion_ExchangeRate">Integración con la API de ExchangeRate</a>
-<a href ="/integracion_covid-api">Integración con la API de datos covid-api</a>
