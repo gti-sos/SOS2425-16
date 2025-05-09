a1a9f1ae8a227926e751eebcdcb9ab37905ec33d
ivabarlay <ivabarlay@proton.me>
2025-05-09 16:20:25 +0200

    Cambiado gráfico worldbank de line a bar

--- src/front/src/routes/integracion_worldbank/+page.svelte
+++ src/front/src/routes/integracion_worldbank/+page.svelte
@@ -16,11 +16,10 @@
 		resultMessage = '';
 
 	function standarizeData(jsonData) {
-        let res = jsonData;
+		let res = jsonData;
 		res = res[1];
 		res = res.map((item) => [item.date, item.value]);
-		res.reverse();
-        return res;
+		return res;
 	}
 
 	async function getData() {
@@ -29,7 +28,7 @@
 
 			if (res.status === 200) {
 				worldbank_data = await res.json();
-                worldbank_data = standarizeData(worldbank_data);
+				worldbank_data = standarizeData(worldbank_data);
 				resultMessage = `Gráfica mostrada`;
 				resultStatus = 'success';
 				// console.log(worldbank_data)
@@ -53,52 +52,24 @@
 		anychart.onDocumentReady(function () {
 			// set chart theme
 			anychart.theme('darkTurquoise');
-			// create data set on our data
-			var dataSet = anychart.data.set(graphData);
+			anychart.theme(anychart.themes.darkEarth);
+			// set the data
+			var data = {
+				header: ['Name', 'Death toll'],
+				rows: graphData
+			};
 
-			// map data for the first series, take x from the zero column and value from the first column of data set
-			var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
+			// create the chart
+			var chart = anychart.bar();
 
-			// map data for the second series, take x from the zero column and value from the second column of data set
-			var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 });
+			// add data
+			chart.data(data);
 
-			// map data for the third series, take x from the zero column and value from the third column of data set
-			var thirdSeriesData = dataSet.mapAs({ x: 0, value: 3 });
+			// set the chart title
+			chart.title('Evolución de la población En África Sur y Este');
 
-			// create line chart
-			var chart = anychart.line();
-
-			// turn on chart animation
-			chart.animation(true);
-
-			// set chart padding
-			chart.padding([10, 20, 5, 20]);
-
-			// turn on the crosshair
-			chart.crosshair().enabled(true).yLabel(false).yStroke(null);
-
-			// set tooltip mode to point
-			chart.tooltip().positionMode('point');
-
-			// set chart title text settings
-			chart.title('Trend of Sales of the Most Popular Products of ACME Corp.');
-
-			// set yAxis title
-			chart.yAxis().title('Number of Bottles Sold (thousands)');
-			chart.xAxis().labels().padding(5);
-
-			// create first series with mapped data
-			var firstSeries = chart.line(firstSeriesData);
-			firstSeries.name('Población de África Este y Sur');
-			firstSeries.hovered().markers().enabled(true).type('circle').size(4);
-			firstSeries.tooltip().position('right').anchor('left-center').offsetX(5).offsetY(5);
-
-			// turn the legend on
-			chart.legend().enabled(true).fontSize(13).padding([0, 0, 10, 0]);
-
-			// set container id for the chart
+			// draw
 			chart.container('container');
-			// initiate chart drawing
 			chart.draw();
 		});
 	});
@@ -125,7 +96,7 @@
 	body,
 	#container {
 		width: 100%;
-		height: 80vh;
+		height: 100vh;
 		margin: 0;
 		padding: 0;
 	}
