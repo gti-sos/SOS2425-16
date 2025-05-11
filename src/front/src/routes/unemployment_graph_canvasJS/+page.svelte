d99e0ceada31a107fb9d22742ebb79b3e490a2e4
NFN8347 <pablovsc2004@gmail.com>
2025-05-11 18:33:16 +0200

    pinceladas finales

--- src/front/src/routes/unemployment_graph_canvasJS/+page.svelte
+++ src/front/src/routes/unemployment_graph_canvasJS/+page.svelte
@@ -13,14 +13,13 @@
 <script>
     import { onMount } from "svelte";
   
-    let chart; // Variable global para el gráfico
+    let chart;
   
     onMount(async () => {
       try {
         const response = await fetch("http://localhost:16078/api/v1/unemployment-stats?year=2021&quarter=q4");
         const data = await response.json();
   
-        // Convertir los datos al formato que CanvasJS necesita
         // @ts-ignore
         const chartData = data.map(entry => ({
           y: entry.unemployment_rate,
@@ -28,7 +27,6 @@
           label: entry.autonomic_community.replace(/-/g, " ").replace(/\b\w/g, l => l.toUpperCase())
         }));
   
-        // Crear el gráfico Doughnut
         // @ts-ignore
         chart = new CanvasJS.Chart("chartContainer", {
           animationEnabled: true,
