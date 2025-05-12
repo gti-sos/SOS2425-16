89e93a1a9ecb6a443d97685068c2b4887d03bd1e
TheVaultBoy16 <gonaguiman@gmail.com>
2025-05-12 09:36:19 +0200

    Arreglado Render Pablo

--- src/front/src/routes/integracion_OpenWeatherMap/+page.svelte
+++ src/front/src/routes/integracion_OpenWeatherMap/+page.svelte
@@ -11,13 +11,19 @@
 
 <script>
     import { onMount } from 'svelte';
+    import { dev } from "$app/environment";
+    let DEVEL_HOST = "http://localhost:16078";
+    let API = "/api/openweather";
+    if(dev){
+        API = DEVEL_HOST + API;
+    }
   
     let chartData = [];
   
     onMount(async () => {
       try {
         // Usamos el proxy
-        const response = await fetch('http://localhost:16078/api/openweather/data/2.5/weather?lat=37.39&lon=-5.99&units=metric');
+        const response = await fetch(`${API}/data/2.5/weather?lat=37.39&lon=-5.99&units=metric`);
         const data = await response.json();
   
         // @ts-ignore
